e350225f31c66bf87fa39735c207c24d4dd7c6d5
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-01-04 11:27:21 +0000

    Added HTML update via JavaScript example

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function helloFunction () {
 	alert ('Hello Napier');
 }
+
+function getDateTime() {
+	var dateTime = Date();
+	document.getElementById('datetime').innerHTML = '<p>' + dateTime + '</p>';
+}
+
+function changeColour(obj, colour) {
+	obj.style.color = colour;
+}
