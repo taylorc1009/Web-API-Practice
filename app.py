f66c071aaf885c42d48d48644d2047ad9a13d919
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-01-11 12:54:24 +0000

    Implemented all other missing endpoint examples

--- app.py
+++ app.py
@@ -16,7 +16,13 @@ stores = [
 
 @app.route('/store', methods=['POST']) # home directory, "https://www.example.com" becomes "https://www.example.com/"
 def create_store():
-	pass
+	request_data = request.get_json()
+	new_store = {
+		'name': request_data['name'],
+		'items': []
+	}
+	stores.append(new_store)
+	return jsonify(new_store)
 
 @app.route('/store')
 def get_stores():
@@ -24,14 +30,29 @@ def get_stores():
 
 @app.route('/store/<string:name>')
 def get_store(name):
-	pass
+	for store in stores:
+		if store['name'] == name:
+			return jsonify(store)
+	return jsonify({'error': 'Store was not found'})
 
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_store_item(name):
-	pass
+	for store in stores:
+		if store['name'] == name:
+			request_data = request.get_json()
+			new_item = {
+				'name': request_data['name']
+				'price': request_data['price']
+			}
+			store['items'].append(new_item)
+			return jsonify(new_item)
+	return jsonify({'error': 'Store was not found'})
 
 @app.route('/store/<string:name>/item')
 def get_store_items(name):
-	pass
+	for store in stores:
+		if store['name'] == name:
+			return jsonify({'items': store['items']})
+	return jsonify({'error': 'Store was not found'})
 
 app.run(port=5000) # if this cuauses an error, the port may be in use so all you need to do is change the port
