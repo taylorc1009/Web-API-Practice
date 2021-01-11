4d0f153b1f1bd3a5ccd68f78cebd28894d4fa2f5
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-01-11 12:07:12 +0000

    Implemented get_stores endpoint body

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -20,17 +20,17 @@ def create_store():
 
 @app.route('/store')
 def get_stores():
-	pass
+	return jsonify({'stores': stores}) # we need to convert 'stores' from a list to a dictionary as 'jsonify' only accepts dictionaries
 
-@app.route('/store/<string: name>')
+@app.route('/store/<string:name>')
 def get_store(name):
 	pass
 
-@app.route('/store/<string: name>/item', methods=['POST'])
+@app.route('/store/<string:name>/item', methods=['POST'])
 def create_store_item(name):
 	pass
 
-@app.route('/store/<string: name>/item')
+@app.route('/store/<string:name>/item')
 def get_store_items(name):
 	pass
 
