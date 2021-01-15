2f086479113da1b4e5d33219aadd6b6755cd9523
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-01-15 14:28:12 +0000

    Moved obsolete code out of active files

---
+++ api/obsolete/security.py
@@ -0,0 +1,30 @@
+### Udemy Section 5
+# def authenticate(username, password): # checks if the user entered the correct password for a username
+    # user = User.find_by_username(username)
+    # if user and safe_str_cmp(user.password, password): # 'safe_str_cmp' is used to compare different character encodings simply: comparing ASCII with Unicode will cause issues
+        # return user
+
+# def identity(payload): # returns the user details, acquired by ID
+    # user_id = payload['identity']
+    # return User.find_by_id(user_id)
+
+
+
+
+
+### Udemy Section 4
+# users = [
+    # User(1, 'bob', 'asdf')
+# ]
+
+# username_mapping = {u.username: u for u in users}
+# userid_mapping = {u.id: u for u in users}
+
+# def authenticate(username, password): # checks if the user entered the correct password for a username
+    # user = username_mapping.get(username, None)
+    # if user and safe_str_cmp(user.password, password): # 'safe_str_cmp' is used to compare different character encodings simply: comparing ASCII with Unicode will cause issues
+        # return user
+
+# def identity(payload): # returns the user details, acquired by ID
+    # user_id = payload['identity']
+    # return userid_mapping.get(user_id, None)
---
+++ api/obsolete/user.py
@@ -0,0 +1,81 @@
+### Udemy Section 5
+# class User:
+    # def __init__(self, _id, username, password):
+        # self.id = _id
+        # self.username = username
+        # self.password = password
+
+    # @classmethod
+    # def find_by_username(cls, username):
+        # connection = sqlite3.connect('data.db')
+        # cursor = connection.cursor()
+
+        # query = "SELECT * FROM users WHERE username=?"
+        # result = cursor.execute(query, (username,)) # the second paramater gives the paramater the query needs ('?'), which should always be given as a tuple
+
+        # row = result.fetchone() # gets the first returned value
+        # if row:
+            # user = cls(*row)
+        # else:
+            # user = None
+
+        # connection.close()
+        # return user
+
+    # @classmethod
+    # def find_by_id(cls, _id):
+        # connection = sqlite3.connect('data.db')
+        # cursor = connection.cursor()
+
+        # query = "SELECT * FROM users WHERE id=?"
+        # result = cursor.execute(query, (_id,))
+
+        # row = result.fetchone()
+        # if row:
+            # user = cls(*row)
+        # else:
+            # user = None
+
+        # connection.close()
+        # return user
+
+# class UserRegister(Resource):
+    # parser = reqparse.RequestParser()
+    # parser.add_argument('username',
+        # type=str,
+        # required=True,
+        # help="Field is either blank or unrecognised."
+    # )
+    # parser.add_argument('password',
+        # type=str,
+        # required=True,
+        # help="Field is either blank or unrecognised."
+    # )
+
+    # def post(self):
+        # request_data = UserRegister.parser.parse_args()
+
+        # if User.find_by_username(request_data['username']):
+            # return {"message": "Username already exists."}, 400
+
+        # connection = sqlite3.connect('data.db')
+        # cursor = connection.cursor()
+
+        # query = "INSERT INTO users VALUES(NULL, ?, ?)"
+        # cursor.execute(query, (request_data['username'], request_data['password']))
+
+        # connection.commit()
+        # connection.close()
+
+        # return {"message": "User created successfully."}, 201
+
+
+
+
+
+### Udemy Section 4
+# class User:
+    # def __init__(self, _id, username, password):
+        # self.id = _id
+        # self.username = username
+        # self.password = password
