401fc1d0571f960ce5dc5d90f2a8d48547e962cd
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-01-16 14:53:50 +0000

    Improved 'Item' endpoints to only work with items in a specific store

--- api/models/item.py
+++ api/models/item.py
@@ -1,15 +1,14 @@
 from db import db
 
 class ItemModel(db.Model):
-    # SQLAlchemy attributes; look at 'user.py' for the implementation descriptions
     __tablename__ = 'items'
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    price = db.Column(db.Float(precision=2)) # 'precision' limits the number entered to 2 decimal places
+    price = db.Column(db.Float(precision=2))
 
-    # relationship with 'stores' table
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.id')) # so we can say which store the item belongs too
-    store = db.relationship('StoreModel') # essentially, this is an SQLAlchemy way of performing a 'JOIN'
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
 
     def __init__(self, name, price, store_id):
         self.name = name
@@ -17,16 +16,57 @@ class ItemModel(db.Model):
         self.store_id = store_id
 
     def json(self):
-        return {'name': self.name, 'price': self.price}
+        return {'name': self.name, 'price': self.price, 'store_id': self.store_id}
 
     @classmethod
-    def find_by_name(cls, name):
-        return cls.query.filter_by(name=name).first() # does the same thing as 'Item.find_by_name()' from the old code saved in 'item.py - Udemy Section 5'
+    def find_item_in_store(cls, name, store_id):
+        from models.store import StoreModel
+        store = StoreModel.query.filter_by(id=store_id).first()
+        if store:
+            return store.items.filter_by(name=name).first()
 
     def save_to_database(self):
-        db.session.add(self) # performs an UPDATE query instead of an INSERT so we can now use thos method for both events
+        db.session.add(self)
         db.session.commit()
 
     def delete_from_database(self):
         db.session.delete(self)
         db.session.commit()
+
+
+
+
+
+### Udemy Section 6
+# from db import db
+
+# class ItemModel(db.Model):
+    # # SQLAlchemy attributes; look at 'user.py' for the implementation descriptions
+    # __tablename__ = 'items'
+    # id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String(80))
+    # price = db.Column(db.Float(precision=2)) # 'precision' limits the number entered to 2 decimal places
+
+    # # relationship with 'stores' table
+    # store_id = db.Column(db.Integer, db.ForeignKey('stores.id')) # so we can say which store the item belongs too
+    # store = db.relationship('StoreModel') # essentially, this is an SQLAlchemy way of performing a 'JOIN'
+
+    # def __init__(self, name, price, store_id):
+        # self.name = name
+        # self.price = price
+        # self.store_id = store_id
+
+    # def json(self):
+        # return {'name': self.name, 'price': self.price}
+
+    # @classmethod
+    # def find_by_name(cls, name):
+        # return cls.query.filter_by(name=name).first() # does the same thing as 'Item.find_by_name()' from the old code saved in 'item.py - Udemy Section 5'
+
+    # def save_to_database(self):
+        # db.session.add(self) # performs an UPDATE query instead of an INSERT so we can now use thos method for both events
+        # db.session.commit()
+
+    # def delete_from_database(self):
+        # db.session.delete(self)
+        # db.session.commit()
--- api/resources/item.py
+++ api/resources/item.py
@@ -3,23 +3,22 @@ from flask_jwt import jwt_required
 from models.item import ItemModel
 
 class Item(Resource):
-    parser = reqparse.RequestParser() # parses the JSON data recieved in the request (it isn't defined here, Flask will find it automatically)
-    # request arguments that are expected for the 'ItemModel'
+    parser = reqparse.RequestParser()
     parser.add_argument('price',
         type=float,
-        required=True,
-        help="Field is either blank or unrecognised."
     )
     parser.add_argument('store_id',
         type=int,
         required=True,
-        help="Field is either blank or unrecognised - items must have a store ID."
+        help="Field cannot be blank - items must have a store ID."
     )
 
     @jwt_required()
     def get(self, name):
+        request_data = Item.parser.parse_args()
+
         try:
-            item = ItemModel.find_by_name(name)
+            item = ItemModel.find_item_in_store(name, request_data['store_id'])
         except:
             return {"message": "An error occurred while reading the item from the database."}, 500
 
@@ -28,13 +27,16 @@ class Item(Resource):
         return {'message': 'Item not found.'}, 404
 
     def post(self, name):
+        request_data = Item.parser.parse_args()
+
+        if not request_data['price']:
+            return {'message': 'Field cannot be blank - items must have a price.'}
+
         try:
-            if ItemModel.find_by_name(name): # we can't do "if self.get(name)" because the 'get' method required a JWT token, so the code to find the item had been moved to 'find_by_name'
-                return {'message': "An item with name '{}' already exists.".format(name)}, 400
+            if ItemModel.find_item_in_store(name, request_data['store_id']):
+                return {'message': "An item with name '{}' already exists in this store.".format(name)}, 400
         except:
             return {"message": "An error occurred while reading the item from the database."}, 500
-
-        request_data = Item.parser.parse_args()
 
         item = ItemModel(name, **request_data)
 
@@ -45,9 +47,11 @@ class Item(Resource):
         return item.json(), 201
 
     def delete(self, name):
+        request_data = Item.parser.parse_args()
+
         try:
             try:
-                item = ItemModel.find_by_name(name)
+                item = ItemModel.find_item_in_store(name, request_data['store_id'])
             except:
                 return {"message": "An error occurred while reading the item from the database."}, 500
 
@@ -61,13 +65,16 @@ class Item(Resource):
         request_data = Item.parser.parse_args()
 
         try:
-            item = ItemModel.find_by_name(name)
+            item = ItemModel.find_item_in_store(name, request_data['store_id'])
         except:
             return {"message": "An error occurred while reading the item from the database."}, 500
 
         if item is None:
             item = ItemModel(name, **request_data)
         else:
+            if not request_data['price']:
+                return {'message': 'Field cannot be blank - items must have a price.'}
+
             item.price = request_data['price']
             item.store_id = request_data['store_id']
 
@@ -78,6 +85,95 @@ class Item(Resource):
 
         return item.json()
 
-class Items(Resource): # used to 'GET' a list of all items
+class Items(Resource):
     def get(self):
-        return {'items': [item.json() for item in ItemModel.query.all()]} # instead of list comprehension, you can also use 'list(map(lambda x: x.json(), ItemModel.query.all()))' (usually used when you need to interact with other languages as, notice how we use the 'map()' conversion, we can convert it to other data types)
+        return {'items': [item.json() for item in ItemModel.query.all()]}
+
+
+
+
+
+### Udemy Section 6
+# from flask_restful import Resource, reqparse
+# from flask_jwt import jwt_required
+# from models.item import ItemModel
+
+# class Item(Resource):
+    # parser = reqparse.RequestParser() # parses the JSON data recieved in the request (it isn't defined here, Flask will find it automatically)
+    # # request arguments that are expected for the 'ItemModel'
+    # parser.add_argument('price',
+        # type=float,
+        # required=True,
+        # help="Field is either blank or unrecognised."
+    # )
+    # parser.add_argument('store_id',
+        # type=int,
+        # required=True,
+        # help="Field is either blank or unrecognised - items must have a store ID."
+    # )
+
+    # @jwt_required()
+    # def get(self, name):
+        # try:
+            # item = ItemModel.find_by_name(name)
+        # except:
+            # return {"message": "An error occurred while reading the item from the database."}, 500
+
+        # if item:
+            # return item.json()
+        # return {'message': 'Item not found.'}, 404
+
+    # def post(self, name):
+        # try:
+            # if ItemModel.find_by_name(name): # we can't do "if self.get(name)" because the 'get' method required a JWT token, so the code to find the item had been moved to 'find_by_name'
+                # return {'message': "An item with name '{}' already exists.".format(name)}, 400
+        # except:
+            # return {"message": "An error occurred while reading the item from the database."}, 500
+
+        # request_data = Item.parser.parse_args()
+
+        # item = ItemModel(name, **request_data)
+
+        # try:
+            # item.save_to_database()
+        # except:
+            # return {"message": "An error occurred while inserting the item to the database."}, 500
+        # return item.json(), 201
+
+    # def delete(self, name):
+        # try:
+            # try:
+                # item = ItemModel.find_by_name(name)
+            # except:
+                # return {"message": "An error occurred while reading the item from the database."}, 500
+
+            # if item:
+                # item.delete_from_database()
+        # except:
+            # return {"message": "An error occurred while deleting the item from the database."}, 500
+        # return {'message': 'Item deleted.'}
+
+    # def put(self, name):
+        # request_data = Item.parser.parse_args()
+
+        # try:
+            # item = ItemModel.find_by_name(name)
+        # except:
+            # return {"message": "An error occurred while reading the item from the database."}, 500
+
+        # if item is None:
+            # item = ItemModel(name, **request_data)
+        # else:
+            # item.price = request_data['price']
+            # item.store_id = request_data['store_id']
+
+        # try:
+            # item.save_to_database()
+        # except:
+            # return {"message": "An error occurred while inserting the item to the database."}, 500
+
+        # return item.json()
+
+# class Items(Resource): # used to 'GET' a list of all items
+    # def get(self):
+        # return {'items': [item.json() for item in ItemModel.query.all()]} # instead of list comprehension, you can also use 'list(map(lambda x: x.json(), ItemModel.query.all()))' (usually used when you need to interact with other languages as, notice how we use the 'map()' conversion, we can convert it to other data types)
